75b80671b89254b3ff9e16450753959eb1fff448
david.zeugner@gmail.com <>
2024-04-01 15:01:38 +0200

    Move train forward (in the direction it is facing) using RigidBody Components

--- Assets/Scripts/TrainScript.cs
+++ Assets/Scripts/TrainScript.cs
@@ -5,15 +5,17 @@ using static UnityEngine.GraphicsBuffer;
 
 public class TrainScript : MonoBehaviour
 {
+    private Rigidbody2D train_RigidBody;
     private bool isDriving;
     public float speed;
-
-    public Transform target;
     
 
     // Start is called before the first frame update
     void Start()
     {
+        // Get RigidBody Component
+        train_RigidBody = GetComponent<Rigidbody2D>();
+        
         isDriving = false;
     }
 
@@ -22,9 +24,11 @@ public class TrainScript : MonoBehaviour
     {
         if (isDriving)
         {
-            float step = speed * Time.deltaTime;
-            transform.position = Vector2.MoveTowards(transform.position, target.position, step);
-            
+            // Always move the train forward in the direction it is facing
+
+            // See https://discussions.unity.com/t/transform-forward-in-2d/182904
+            Vector2 direction = -transform.right;
+            train_RigidBody.MovePosition(train_RigidBody.position + direction * Time.deltaTime * speed);     
         }
     }
 
@@ -36,7 +40,6 @@ public class TrainScript : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        target = collision.transform;
         if (transform.rotation.z != collision.transform.rotation.z) {
             isDriving = false;
         }
