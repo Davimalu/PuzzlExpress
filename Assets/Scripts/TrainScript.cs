f04a6726cdd64c3c890a549306b2835bdb3f1e3f
david.zeugner@gmail.com <>
2024-04-01 21:15:09 +0200

    Fixed various bugs where the train wouldn't follow the rails + Refactor Code

--- Assets/Scripts/TrainScript.cs
+++ Assets/Scripts/TrainScript.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,14 +10,14 @@ public class TrainScript : MonoBehaviour
     private bool isDriving;
     private int collisionCount = 0;
     public float speed;
-    
+
 
     // Start is called before the first frame update
     void Start()
     {
         // Get RigidBody Component
         train_RigidBody = GetComponent<Rigidbody2D>();
-        
+
         isDriving = false;
     }
 
@@ -30,7 +31,7 @@ public class TrainScript : MonoBehaviour
 
             // See https://discussions.unity.com/t/transform-forward-in-2d/182904
             Vector2 direction = -transform.right;
-            train_RigidBody.MovePosition(train_RigidBody.position + direction * Time.deltaTime * speed);     
+            train_RigidBody.MovePosition(train_RigidBody.position + direction * Time.deltaTime * speed);
         }
     }
 
@@ -42,42 +43,95 @@ public class TrainScript : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        // See https://discussions.unity.com/t/why-does-transform-rotate-behave-so-strange/120303
+        double trainRotation = Mathf.Round(transform.rotation.eulerAngles.z) % 360;
+        double colliderRotation = Mathf.Round(collision.transform.rotation.eulerAngles.z) % 360;
+
         collisionCount++;
-        
+
         // Collision with straigt rail
-        if (collision.gameObject.tag.Equals("straightRail") == true) {
+        if (collision.gameObject.tag.Equals("straightRail") == true)
+        {
             // If the rotation of the rail is different than the rotation of the train -> Stop the train
-            if (transform.rotation.z != collision.transform.rotation.z) {
+            // Distinguish whether the train is travelling horizontally or vertically
+
+            if (trainRotation != colliderRotation && trainRotation != colliderRotation - 180 && trainRotation != colliderRotation + 180)
+            {
+                Debug.Log("Stop A");
+                Debug.Log(trainRotation);
+                Debug.Log(colliderRotation);
                 isDriving = false;
             }
         }
-        
+
         // Collision with curve rail
-        if (collision.gameObject.tag.Equals("curveRail") == true) {
+        if (collision.gameObject.tag.Equals("curveRail") == true)
+        {
             // If the rotation of the curve rail is different than the rotation of the train -> Stop the train 
-            // There are 2 possible rotations in which the curve can be entered by the train
-            // See https://discussions.unity.com/t/why-does-transform-rotate-behave-so-strange/120303
-            if (transform.rotation.z != collision.transform.rotation.z && transform.rotation.eulerAngles.z != collision.transform.rotation.eulerAngles.z - 270) {
+            // Distinguish whether the train is travelling horizontally or vertically
+
+            if (trainRotation != colliderRotation && trainRotation != colliderRotation - 270 && trainRotation != colliderRotation + 90)
+            {
+                Debug.Log("Stop C");
+                Debug.Log(trainRotation);
+                Debug.Log(colliderRotation);
                 isDriving = false;
             }
         }
 
         // Collision with direction changer of curveRail
-        if (collision.gameObject.tag.Equals("directionChanger") == true) {
+        /* Note: The child of Train (Center Collider) and Curve Rail (Direction Changer) are both on a different layer (curveCollider)
+           This ensures that the middle of the train will collide with the middle of the curve */
+        if (collision.gameObject.tag.Equals("directionChanger") == true)
+        {
+            // Distinguish whether the train is travelling horizontally or vertically
             // Depending on the side on which the train enters the curve, change direction differently
-            float rotationDirection = transform.rotation.z;
+            Quaternion rotationDirection = Quaternion.Euler(0, 0, 0);
 
-            if (transform.rotation.z == collision.transform.rotation.z) {
-                Debug.Log("Side A");
-                rotationDirection = collision.transform.rotation.eulerAngles.z - 90;
+            if (trainRotation == 0 || trainRotation == 180)
+            {
+                if (trainRotation == colliderRotation || trainRotation == colliderRotation + 90)
+                {
+                    // Note: Use quaternions instead of degrees to avoid floating point inaccuracies
+                    rotationDirection = Quaternion.Euler(0, 0, 270);
+                }
+
+                if (trainRotation == colliderRotation - 270)
+                {
+                    rotationDirection = Quaternion.Euler(0, 0, 90);
+                }
+            }
+            else
+            {
+                if (trainRotation == colliderRotation)
+                {
+                    if (trainRotation == 90)
+                    {
+                        Debug.Log("A");
+                        rotationDirection = Quaternion.Euler(0, 0, 0);
+                    }
+                    if (trainRotation == 270)
+                    {
+                        Debug.Log("B");
+                        rotationDirection = Quaternion.Euler(0, 0, 180);
+                    }
+                }
+
+                if (trainRotation == colliderRotation - 270 || trainRotation == colliderRotation + 90)
+                {
+                    if (trainRotation == 90)
+                    {
+                        rotationDirection = Quaternion.Euler(0, 0, 180);
+                    }
+                    if (trainRotation == -90)
+                    {
+                        rotationDirection = Quaternion.Euler(0, 0, 0);
+                    }
+                }
             }
 
-            if (transform.rotation.eulerAngles.z == collision.transform.rotation.eulerAngles.z - 270) {
-                Debug.Log("Side B");
-                rotationDirection = collision.transform.rotation.eulerAngles.z + 180; 
-            }
-
-            train_RigidBody.SetRotation(rotationDirection); 
+            transform.rotation = rotationDirection;
+            // train_RigidBody.SetRotation(rotationDirection);
         }
     }
     private void OnTriggerExit2D(Collider2D collision)
