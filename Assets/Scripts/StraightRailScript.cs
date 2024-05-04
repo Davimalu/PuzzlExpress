17e93213a9f1665407a406ffe7a196dd00a0e4f8
david.zeugner@gmail.com <>
2024-05-04 08:43:09 +0200

    Rail that the train is standing on cannot be rotated and implement junctions

--- Assets/Scripts/StraightRailScript.cs
+++ Assets/Scripts/StraightRailScript.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class StraightRailScript : MonoBehaviour
 {
     private TrainScript trainScript;
+    private bool collidingWithTrain;
 
     // Start is called before the first frame update
     void Start()
@@ -22,12 +23,28 @@ public class StraightRailScript : MonoBehaviour
     {
         Debug.Log("Klick auf gerade Schiene");
 
-        // Rotation only possible if the train is not driving
-        if (trainScript.isDriving == false)
+        // Rotation only possible if the train is not driving and the train is not standing on that track
+        if (trainScript.isDriving == false && collidingWithTrain == false)
         {
             // See https://discussions.unity.com/t/add-90-degrees-to-transform-rotation/31852
             Quaternion rotationDirection = transform.rotation * Quaternion.Euler(0, 0, 90);
             transform.rotation = rotationDirection;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag.Equals("train") == true)
+        {
+            collidingWithTrain = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag.Equals("train") == true)
+        {
+            collidingWithTrain = false;
         }
     }
 }
--- Assets/Scripts/switchScript.cs
+++ Assets/Scripts/switchScript.cs
@@ -8,25 +8,33 @@ public class switchScript : MonoBehaviour
     public SpriteRenderer spriteRenderer;
     public Sprite pos1;
     public Sprite pos2;
+    private TrainScript trainScript;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        trainScript = GameObject.FindGameObjectWithTag("train").GetComponent<TrainScript>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (switched == true) {
+        if (switched == true)
+        {
             spriteRenderer.sprite = pos2;
-        } else {
+        }
+        else
+        {
             spriteRenderer.sprite = pos1;
         }
     }
 
     private void OnMouseDown()
     {
-        switched = !switched;
+        // Switching only possible if the train is not driving
+        if (trainScript.isDriving == false)
+        {
+            switched = !switched;
+        }
     }
 }
