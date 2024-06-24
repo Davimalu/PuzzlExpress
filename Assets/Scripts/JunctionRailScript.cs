30193e07ce1143a6bde0e9aa58835d2100481625
david.zeugner@gmail.com <>
2024-06-24 13:33:56 +0200

    Fix Game Over Bug in Level 17

--- Assets/Scripts/JunctionRailScript.cs
+++ Assets/Scripts/JunctionRailScript.cs
@@ -14,18 +14,19 @@ public class JunctionRailScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-    
+
     }
 
     // Enables collider after 1 sec
     static IEnumerator EnableColidderAgain(Collider2D collision)
     {
-        yield return new WaitForSeconds(1f);
+        yield return new WaitForSeconds(0.5f);
         collision.GetComponent<Collider2D>().enabled = true;
     }
 
-    void OnTriggerExit2D() {
+    void OnTriggerExit2D()
+    {
         // Enables junction rails collider again, since otherwise the train wouldnt be able to drive on it again
-        StartCoroutine(EnableColidderAgain(this.GetComponent<Collider2D>())); 
+        StartCoroutine(EnableColidderAgain(this.GetComponent<Collider2D>()));
     }
 }
